6c42b1f062d670aa653b72e50d204b9e96248092
takada-eviry <takada@eviry.com>
2022-10-24 13:58:13 +0900

    Initial commit

---
+++ test/fetch-youtube-data-statemachine.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FetchYoutubeDataStatemachine from '../lib/fetch-youtube-data-statemachine-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fetch-youtube-data-statemachine-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FetchYoutubeDataStatemachine.FetchYoutubeDataStatemachineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
